cfc75a5c1dc4b5216d2843a426c2e226007e79e9
Jesus Carrero <jesusenrique.carrero@gmail.com>
2024-10-21 20:47:20 -0400

    Change of text in the footer

--- src/components/sellOrder/FooterSellOrder.tsx
+++ src/components/sellOrder/FooterSellOrder.tsx
@@ -7,8 +7,7 @@ export default function FooterSellOrder({ className = "" }: FooterSellOrderProps
     <>
       <div className={"FooterSellOrder " + className}>
         <div className="notes text-sm">
-          <p>NOTA: El pago de esta mercancía se recibirá en Dólares Americanos USD o en Pesos Colombianos, tal y como se especifica en cada renglón recibido en su Descripción.</p>
-          <b>NOTA: Pago en Bolívares a Tasa promedio al momento de la transferencia.</b>
+          <p>NOTA: El pago de esta mercancía se puede recibir en Dólares Americanos USD o en Pesos Colombianos, tal y como se especifica en cada renglón recibido en su descripción.</p>
           <p>NOTA: La condición de tiempo de pago máximo será de 7 días contínuos a partir de la fecha de recepción de esta nota de entrega. <b>LA FACTURA SE ELABORA AL  MOMENTO DEL PAGO</b></p>
         </div>
 
